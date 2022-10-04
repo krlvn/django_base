2961b5838d865a8e493f9f2dd625bacfbcb8bc2e
krlvn <krlvn39@gmail.com>
2022-10-04 13:58:26 +0200

    Добавил namespace и media

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,7 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from mainapp.apps import MainappConfig
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainapp.urls')),
+    path('', include('mainapp.urls', namespace=MainappConfig.name)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
